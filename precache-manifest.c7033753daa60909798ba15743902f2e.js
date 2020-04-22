7d97d5d90ee8ef027c77fa089e69ed2c38ae6def
Ben Abbott <babbott85@gmail.com>
2020-04-22 15:36:27 -0600

    Updates

---
+++ precache-manifest.c7033753daa60909798ba15743902f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071fbe092edd5cfa5c898d482feca2e7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fe40febdaa68504e7017",
+    "url": "/portfolio/static/css/main.3afadc59.chunk.css"
+  },
+  {
+    "revision": "12eb8b67165eba596c96",
+    "url": "/portfolio/static/js/2.227a228a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.227a228a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe40febdaa68504e7017",
+    "url": "/portfolio/static/js/main.3421c0bb.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
