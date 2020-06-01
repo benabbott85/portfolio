c92dca58a49e6c4b3193dbb9b396a0f1f115ccaf
Ben Abbott <babbott85@gmail.com>
2020-06-01 11:11:05 -0600

    updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,7 +10,7 @@ function About() {
       <p> I was born in New York City on May 13, 1988, and yes that was Friday the 13th. I am the second of two boys,
               my
               older brother is Zach.
-              My father has been a practicing labor lawyer for over 40 years working mostly with unions. My mom was a stay
+              My father was been a practicing labor lawyer for over 40 years working mostly with unions. My mom was a stay
               at home mom and also
               a choreographer on the side.
               I grew up in a small town, Point Lookout, on the south shore of Long Island about an hour east of the city.
@@ -19,14 +19,11 @@ function About() {
           <p> After high school, I left New York to attend the University of Colorado at Boulder. I studied communication
               and
               graduated in 2010 with a
-              Bachelor of Arts. Following college, I moved to Park City, Utah to enjoy a year as a ski bum. It was
-              probably
-              the best year of my life.
-              I worked in a restaurant and skiied over 100 days.
+              Bachelor of Arts. Following college, I moved to Park City, Utah to enjoy a year as a ski bum.
           </p>
   
           <p> After my ski bum year ended, I moved back to New York and settled in Williamsburg, a section of Brooklyn. I
-              worked in the city doing benefits
+              worked in New York City doing benefits
               administration for collectively bargained union health and pension funds. After about 2.5 years of that, I
               moved
               back to Denver because it seemed
@@ -35,10 +32,10 @@ function About() {
               Colorado.
           </p>
   
-          <p> I decided to get into coding while at my last job. The company ran their own coding boot camp and that is
-              what
-              first truly made me think about it.
-              After leaving that company I came to the decision that this was the best decision for me and for my future.
+          <p> I first started to think about web development as my future while at my previous job. They run a coding boot camp of their own
+            and the more I started to think about my future, the more it made sense to make a change. I have always had a love of technology
+            and how it continues to evolve. I enrolled in the full stack web development certificate program at the University of Denver and 
+            that is where my passion for coding truly grew. 
           </p>
 
       </div>
